cd1035fdfa88d2cee9dc31d2faf8c550b6f22fba
Justine <Justinegeechu333@gmail.com>
2023-10-10 09:24:46 -0500

    step1

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React from "react";
-import Header from "./Header";
-import ListingsContainer from "./ListingsContainer";
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import ListingsContainer from './ListingsContainer';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <ListingsContainer />
-    </div>
-  );
+	const [listings, setListings] = useState([]);
+	useEffect(() => {
+		const loadPage = () => {
+			const url = 'http://localhost:6001/listings';
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					setListings(data);
+				})
+				.catch(error => console.error(error));
+		};
+		loadPage();
+	}, []);
+	console.log(listings);
+	return (
+		<div className='app'>
+			<Header />
+			<ListingsContainer listings={listings} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,26 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React from 'react';
+import ListingCard from './ListingCard';
 
-function ListingsContainer() {
-  return (
-    <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
-    </main>
-  );
+function ListingsContainer({ listings }) {
+	console.log(listings);
+	return (
+		<main>
+			<ul className='cards'>
+				{listings.map(listing => {
+					return (
+						<ListingCard
+							key={listing.id}
+							description={listing.description}
+							image={listing.image}
+							location={listing.location}
+						/>
+					);
+				})}
+
+				{/* use the ListingCard component to display listings */}
+			</ul>
+		</main>
+	);
 }
 
 export default ListingsContainer;
